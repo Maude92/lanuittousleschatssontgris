1c61bde0708c996b0b1e0a1f929c82f3aeb7aa4e
AthenaTyrell <36172699+AthenaTyrell@users.noreply.github.com>
2018-03-01 15:08:17 -0500

    TheRaccoonIsSlowlyWorking

--- Assets/RaccoonAnimation.cs
+++ Assets/RaccoonAnimation.cs
@@ -6,18 +6,23 @@ public class RaccoonAnimation : MonoBehaviour {
 	GameObject Raton;
 	Animation anim;
 	bool isiddle = true;
-	AnimationClip[] animclip;
+	//AnimationClip[] animclip;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animation> ();
-		animclip = GetComponent<AnimationClip[]> ();
+		//animclip = GetComponent<AnimationClip[]> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		print (isiddle);
+		if(Input.GetKeyDown(KeyCode.Y)){
+			isiddle = !isiddle;
+		}
 		if (isiddle == true) {
-			
+			anim.Play("Armature.001|idle_2");
 		} else {
+			anim.Play("Armature.001|run_2");
 		}
 	}
 }
