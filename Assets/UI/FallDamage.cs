61308b9b4975b68d12d9e60ae0ba1abe810168f7
Flx <felix.leblancv4si@gmail.com>
2018-03-01 10:35:42 -0500

    UI + Animation saut vers le bas
    On essaye de pas tout chier.

---
+++ Assets/UI/FallDamage.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FallDamage : MonoBehaviour {
+    public Transform healthbar;
+    public float startYPos;
+    public float endYPos;
+    public float damageThreshold = 10;
+    public bool damageMe = false;
+    public bool firstCall = true;
+	public GameObject Mist;
+
+	CubeGrounded cubegrounded;
+
+	void Start () {
+		cubegrounded = GetComponent <CubeGrounded> ();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (cubegrounded.isGrounded == false)
+        {
+            if (Mist.transform.position.y > startYPos)
+            {
+                firstCall = true;
+            }
+            if (firstCall)
+            {
+                startYPos = gameObject.transform.position.y;
+                firstCall = false;
+                damageMe = true;
+            }
+        }
+		if (cubegrounded.isGrounded == true)
+        {
+            endYPos = Mist.transform.position.y;
+            if (startYPos - endYPos > damageThreshold)
+            {
+                if (damageMe)
+                {
+                    //healthbar.GetComponent<HealthBar>().Damage(startYPos - endYPos - damageThreshold);
+					healthbar.GetComponent<HealthBar>().Damage(startYPos - endYPos);
+                    damageMe = false;
+                    firstCall = true;
+                }                
+            }
+        }
+	}
+}
