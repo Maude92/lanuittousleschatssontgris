834dc3edd46f60626b2866f2f5fdc2db13c9ab4d
Maude92 <36176462+Maude92@users.noreply.github.com>
2018-03-09 21:33:59 -0500

    TutoSceneSansOiseau
    La scène de tuto est prête pour recevoir l'oiseau! La bonne scène s'appelle Test_Tutoriel_Maude_v3.

---
+++ Assets/Script/Hurt.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurt : MonoBehaviour {
+
+	public GameObject vieObj;
+	HealthBar viescript;
+	//Animator anim;
+
+//	private AudioManager audioManager;
+
+	// Use this for initialization
+	void Start () {
+		viescript = vieObj.GetComponent<HealthBar> ();
+		//anim = X.GetComponent<Animator> ();
+
+//		audioManager = AudioManager.instance;
+//		if (audioManager == null) {
+//			Debug.LogError ("Attention, le AudioManager n'a pas été trouvé dans la scène.");}
+	}
+
+	IEnumerator TurnOffTrigger(){
+		Debug.Log ("HURT");
+		GetComponent<Collider> ().enabled = false;
+		yield return new WaitForSeconds (2);
+		GetComponent<Collider> ().enabled = true;
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player" && viescript.currentHealth >= 1.0f) {
+			//audioManager.PlaySound ("Hurt");
+			//anim.SetBool ("Hit", true);
+			viescript.currentHealth = viescript.currentHealth - 5.0f;
+			StartCoroutine (TurnOffTrigger());
+		}
+	}
+}
