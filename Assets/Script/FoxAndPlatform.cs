f048f5435db17f3789e3ca7420b63a5d1a186f27
Maude92 <36176462+Maude92@users.noreply.github.com>
2018-03-19 01:07:36 -0400

    SuiteConstructionTuto
    Y'a maintenant un début de bout sur les toits. Yay!

---
+++ Assets/Script/FoxAndPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxAndPlatform : MonoBehaviour {
+
+	void OnTriggerStay (Collider other){
+		if (other.gameObject.tag == "Platform") {
+			transform.parent = other.transform;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.gameObject.tag == "Platform") {
+			transform.parent = null;
+		}
+	}
+
+}
