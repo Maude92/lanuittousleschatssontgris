ea7f31444053453314a44bc6dce54b80f8fe1ebc
AthenaTyrell <36172699+AthenaTyrell@users.noreply.github.com>
2018-03-19 13:54:58 -0400

    CheckPoints

---
+++ Assets/MoveMistToCheckPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMistToCheckPoint : MonoBehaviour {
+	public GameObject CheckPoint;
+
+	public GameObject Prefab;
+	public Transform InstatiatePlatform;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			other.transform.position = CheckPoint.transform.position;
+			Instantiate (Prefab, InstatiatePlatform.transform.position, InstatiatePlatform.transform.rotation);
+			//transform.parent = other.transform;
+		}
+	}
+
+
+}
