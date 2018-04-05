bdb6331deda6706f5a273e49d3dc0a4ed650f3ff
Flx <felix.leblancv4si@gmail.com>
2018-04-05 19:09:47 -0400

    A lot of things
    UI, particle, Tag, Layer, etc.

--- Assets/UI/HealthBar.cs
+++ Assets/UI/HealthBar.cs
@@ -10,7 +10,7 @@ public class HealthBar : MonoBehaviour {
 
 	public Transform character;
     public Transform HBImage;
-    public Transform HBText;
+    //public Transform HBText;
     public float maxHealth;
     public float currentHealth;
     public float health;
@@ -34,6 +34,9 @@ public class HealthBar : MonoBehaviour {
 	public Image Vie8;
 	public Image Vie9;
 
+	public Image Circle;
+	public CanvasGroup UIvie;
+
 	// Use this for initialization
 	void Start () {
 		audioManager = AudioManager.instance;
@@ -42,28 +45,45 @@ public class HealthBar : MonoBehaviour {
 		}
 
 		HBImage = gameObject.transform.GetChild(0).GetChild(0);
-        HBText = gameObject.GetComponentInChildren<Text>().transform;
+       //Texte
+		//HBText = gameObject.GetComponentInChildren<Text>().transform;
         HBImage.GetComponent<Image>().fillAmount = 1;
 		HBImage.GetComponent<Image> ().color = fullColor;
         currentHealth = maxHealth;
 
+	//Circle
+		//Circle.color = HBImage.GetComponent<Image> ().color;
+		Circle.fillAmount = 1;
+
 		NbVieRestant = 9;
 
+		UIvie.alpha = 0;
+
 		//LowVieSound.enabled = false;
+
+
 	}
 	
     public void Damage(float damageAmount)
     {
+
+		UIvie.alpha = 1;
+
 		audioManager.PlaySound ("Mist_Damage");
 		health = (currentHealth - damageAmount) / maxHealth;
         currentHealth -= damageAmount;
         currentHealth = Mathf.Round(currentHealth);
         HBImage.GetComponent<Image>().fillAmount = health;
 		HBImage.GetComponent<Image> ().color = Color.Lerp (lowColor, fullColor, HBImage.GetComponent<Image>().fillAmount);
-        HBText.GetComponent<Text>().text = currentHealth.ToString();
+
+		//Circle.color = HBImage.GetComponent<Image> ().color;
+        
+	//Texte
+		//HBText.GetComponent<Text>().text = currentHealth.ToString();
         if (health <= 0)
         {
-            HBText.GetComponent<Text>().text = "0";
+           //Texte
+			//HBText.GetComponent<Text>().text = "0";
 			NbVieRestant--;
 			if (NbVieRestant > 0) {
 				//Fade to black
@@ -72,7 +92,11 @@ public class HealthBar : MonoBehaviour {
 				health = 100;
 				HBImage.GetComponent<Image>().fillAmount = health;
 				HBImage.GetComponent<Image> ().color = Color.Lerp (lowColor, fullColor, HBImage.GetComponent<Image>().fillAmount);
-				HBText.GetComponent<Text>().text = currentHealth.ToString();
+
+				//Circle.color = HBImage.GetComponent<Image> ().color;
+
+			//Texte
+				//HBText.GetComponent<Text>().text = currentHealth.ToString();
 			}
 
             //GameObject.DestroyObject(character.gameObject);
@@ -89,6 +113,9 @@ public class HealthBar : MonoBehaviour {
     }
 
 	public void LifeGain (float gainAmount){
+
+		UIvie.alpha = 1;
+
 		audioManager.PlaySound ("Mist_Eating");
 		currentHealth = (currentHealth + gainAmount);
 		if (currentHealth > 100) {
@@ -98,7 +125,11 @@ public class HealthBar : MonoBehaviour {
 		currentHealth = Mathf.Round(currentHealth);
 		HBImage.GetComponent<Image>().fillAmount = healtGain;
 		HBImage.GetComponent<Image> ().color = Color.Lerp (lowColor, fullColor, HBImage.GetComponent<Image>().fillAmount);
-		HBText.GetComponent<Text>().text = currentHealth.ToString();
+
+		//Circle.color = HBImage.GetComponent<Image> ().color;
+
+	//Texte
+		//HBText.GetComponent<Text>().text = currentHealth.ToString();
 
 		if (currentHealth > 25) {
 			LowVieSound.Stop ();
@@ -107,6 +138,8 @@ public class HealthBar : MonoBehaviour {
 	}
 
 	void Update () {
+
+		StartCoroutine("FadeOut");
 
 
 		if (currentHealth <= 25 && !LowVieSound.isPlaying) {
@@ -144,6 +177,8 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = true;
 			Vie8.enabled = true;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0.89f;
 		}
 
 //S'il reste 7 Vie :
@@ -157,6 +192,8 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = true;
 			Vie8.enabled = false;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0.78f;
 		}
 
 //S'il reste 6 Vie :
@@ -170,6 +207,8 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = false;
 			Vie8.enabled = false;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0.67f;
 		}
 
 //S'il reste 5 Vie :
@@ -183,6 +222,8 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = false;
 			Vie8.enabled = false;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0.56f;
 		}
 
 //S'il reste 4 Vie :
@@ -196,6 +237,8 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = false;
 			Vie8.enabled = false;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0.45f;
 		}
 
 //S'il reste 3 Vie :
@@ -209,6 +252,8 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = false;
 			Vie8.enabled = false;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0.34f;
 		}
 
 //S'il reste 2 Vie :
@@ -222,6 +267,8 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = false;
 			Vie8.enabled = false;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0.23f;
 		}
 
 //S'il reste 1 Vie :
@@ -235,6 +282,8 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = false;
 			Vie8.enabled = false;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0.12f;
 		}
 
 //S'il reste 0 Vie :
@@ -248,10 +297,38 @@ public class HealthBar : MonoBehaviour {
 			Vie7.enabled = false;
 			Vie8.enabled = false;
 			Vie9.enabled = false;
+
+			Circle.fillAmount = 0;
+
 			//Écran Game Over à faire
 		}
 
 
+	}
+
+	IEnumerator FadeOut() {
+		//yield return new WaitForSeconds (5f);
+
+		float time = 1f;
+
+//		while(UIvie.alpha > 0)
+//		{
+//			yield return new WaitForSeconds (5f);
+//			UIvie.alpha -= Time.deltaTime / time;
+//			yield return null;
+//		}
+
+		if (UIvie.alpha > 0) {
+			yield return new WaitForSeconds (5f);
+			UIvie.alpha -= Time.deltaTime / time;
+			if (UIvie.alpha < 0) {
+				UIvie.alpha = 0;
+			}
+		}
+
+//		if (UIvie.alpha < 0) {
+//			UIvie.alpha = 0;
+//		}
 	}
 
 }
