92da018758873c0bcd852835671a03fb9eb1d01a
Flx <felix.leblancv4si@gmail.com>
2018-04-09 15:35:50 -0400

    Thug Mist is Thug
    Push this.

--- Assets/Script/ThugMist.cs
+++ Assets/Script/ThugMist.cs
@@ -8,10 +8,13 @@ public class ThugMist : MonoBehaviour {
 
 	public GameObject fallingobject;
 	public GameObject mistObj;
-	public GameObject particlesDestruction;
+	//public GameObject particlesDestruction;
 
 	public float timeBeforeFall = 0.4f;
 	public float thugForce = 100f;
+
+	public GameObject DestroyedVersion;
+	public GameObject FlowerOntheGround;
 
 	Animator anim;
 	//Collider colFallingObjet;
@@ -36,21 +39,25 @@ public class ThugMist : MonoBehaviour {
 		
 	}
 
-	void OnTriggerEnter (Collider other){
-		if (other.gameObject.tag == "Player" && gameObject.tag == "FallingObjectDroite" ) {
+	void OnTriggerStay/*Enter*/ (Collider other){
+		if (other.gameObject.tag == "Player" && gameObject.tag == "FallingObjectDroite" && Input.GetButtonDown("XbOne_YButton")) {
 			//print ("Je suis proche du pot de fleur");
+			audioManager.PlaySound ("Flower_Woosh");
 			StartCoroutine (MistEstThug());
 		} 
-		if (other.gameObject.tag == "Player" && gameObject.tag == "FallingObjectGauche") {
+		if (other.gameObject.tag == "Player" && gameObject.tag == "FallingObjectGauche" && Input.GetButtonDown("XbOne_YButton")) {
+			audioManager.PlaySound ("Flower_Woosh");
 			StartCoroutine (MistEstThugGauche());
 		}
 	}
 
 	void OnCollisionEnter (Collision other){
 		if (other.gameObject.tag == "Solide" || other.gameObject.tag == "Gazon") {
-			particlesDestruction.transform.position = gameObject.transform.position;
-			particlesDestruction.SetActive (true);
+			//particlesDestruction.transform.position = gameObject.transform.position;
+			//particlesDestruction.SetActive (true);
 			audioManager.PlaySound ("Flower_Destroy");
+			Instantiate (DestroyedVersion, transform.position, transform.rotation);
+			Instantiate (FlowerOntheGround, transform.position, transform.rotation);
 			Destroy (gameObject);
 		}
 	}
