3e3cfb1de8f529519c540865cce9ede9dd072454
Flx <felix.leblancv4si@gmail.com>
2018-04-14 18:12:15 -0400

    Ecran objectif + ecran perte d'une vie
    Test, réussite et échec.

---
+++ Assets/AnimatedText.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedText : MonoBehaviour {
+
+	private AudioManager audioManager;
+
+	public float delay = 0.1f;
+	public string fullText;
+	private string currentText = "";
+
+	public CanvasGroup InstructionBoutonStart;
+
+	public bool death = true;
+
+	// Use this for initialization
+	void Start () {
+
+		InstructionBoutonStart.alpha = 0.01f;
+		StartCoroutine(ShowText());
+
+		audioManager = AudioManager.instance;
+		if (audioManager == null) {
+			Debug.LogError ("Attention le AudioManager n'est pas détecter dans cette scène");
+		}
+	
+	}
+
+	void Update () {
+		if (Input.GetButtonDown("XbOne_StartButton")) {
+			//Faire reloader le dernier checkpoint ou le début du niveau ?
+			print ("Je dois loader le dernier checkpoint ou faire reloader le level");
+		}
+
+		StartCoroutine(FadeIn());
+	}
+
+	IEnumerator ShowText(){
+//		yield return new WaitForSeconds (0.01f);
+//		audioManager.PlaySound ("GameOver_Screen");
+		//audioManager.PlaySound ("Objectif_New"); //Son pour objectif
+		if (death == true){
+
+			for(int i = 0; i < fullText.Length; i++){
+				currentText = fullText.Substring(0,i);
+				this.GetComponent<Text>().text = currentText;
+				yield return new WaitForSeconds(delay);
+				//audioManager.PlaySound ("GameOver_Screen");
+			}
+			//yield return new WaitForSeconds (0.1f);
+			audioManager.PlaySound ("GameOver_Screen");
+			yield return new WaitForSeconds (1.0f);
+			InstructionBoutonStart.alpha = 0;
+		}
+
+	}
+
+	IEnumerator FadeIn() {
+		//yield return new WaitForSeconds (5f);
+
+		float time = 1f;
+
+		if (InstructionBoutonStart.alpha != 0.01f) {
+			yield return new WaitForSeconds (0.01f);
+			InstructionBoutonStart.alpha += Time.deltaTime * time;
+			if (InstructionBoutonStart.alpha > 1) {
+				InstructionBoutonStart.alpha = 1;
+			}
+		}
+	}
+
+}
---
+++ Assets/FadeOutTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOutTest : MonoBehaviour {
+
+	public CanvasGroup BlackObjectifScreen;
+
+	// Use this for initialization
+	void Start () {
+		BlackObjectifScreen.alpha = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButtonDown("XbOne_StartButton") || Input.GetButton("XbOne_StartButton") || Input.GetButtonUp("XbOne_StartButton")) {
+			StartCoroutine("FadeOut");
+			}
+		}
+
+	IEnumerator FadeOut() {
+		//yield return new WaitForSeconds (5f);
+
+		float time = 1f;
+
+		if (BlackObjectifScreen.alpha > 0) {
+			yield return new WaitForSeconds (0.01f);
+			BlackObjectifScreen.alpha -= Time.deltaTime / time;
+			if (BlackObjectifScreen.alpha < 0) {
+				BlackObjectifScreen.alpha = 0;
+			}
+		}
+	}
+}
