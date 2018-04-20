913a9be0e0114298118d3f6c05c1cd7e2433c830
Flx <felix.leblancv4si@gmail.com>
2018-04-19 20:45:24 -0400

    Et je Push Push Push des Vases pour oublier la honte.
    oui.

---
+++ Assets/ThugMist_V2.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThugMist_V2 : MonoBehaviour {
+
+	private AudioManager audioManager;
+
+	public GameObject fallingobject;
+	public GameObject mistObj;
+	//public GameObject particlesDestruction;
+
+	public float timeBeforeFall = 0.4f;
+	public float thugForce = 100f;
+
+	//public GameObject DestroyedVersion;
+	//public GameObject FlowerOntheGround;
+
+	Animator anim;
+	//Collider colFallingObjet;
+	Rigidbody rbObject;
+
+	// Use this for initialization
+	void Start () {
+		audioManager = AudioManager.instance;
+		if (audioManager == null) {
+			Debug.LogError ("Attention le AudioManager n'est pas détecter dans cette scène");
+		}
+
+
+
+		anim = mistObj.GetComponent<Animator> ();
+		//colFallingObjet = GetComponent<Collider> ();
+		rbObject = fallingobject.GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player" && gameObject.tag == "FallingObjectDroite" && Input.GetButtonDown("XbOne_YButton")) {
+			//print ("Je suis proche du pot de fleur");
+			audioManager.PlaySound ("Flower_Woosh");
+			StartCoroutine (MistEstThug());
+		} 
+		if (other.gameObject.tag == "Player" && gameObject.tag == "FallingObjectGauche" && Input.GetButtonDown("XbOne_YButton")) {
+			audioManager.PlaySound ("Flower_Woosh");
+			StartCoroutine (MistEstThugGauche());
+		}
+	}
+
+	void OnCollisionEnter (Collision other){
+		if (other.gameObject.tag == "Solide" || other.gameObject.tag == "Gazon") {
+			//particlesDestruction.transform.position = gameObject.transform.position;
+			//particlesDestruction.SetActive (true);
+			audioManager.PlaySound ("Flower_Destroy");
+			//Instantiate (DestroyedVersion, transform.position, transform.rotation);
+			//Instantiate (FlowerOntheGround, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+
+	IEnumerator MistEstThug(){
+		anim.SetLayerWeight(3, 1);
+		anim.SetBool ("Punch", true);
+		yield return new WaitForSeconds (timeBeforeFall);
+		rbObject.AddForce (new Vector3 (0, 0, thugForce));
+		//print ("Die! Pot de fleur");
+	}
+
+	IEnumerator MistEstThugGauche(){
+		anim.SetLayerWeight(3, 1);
+		anim.SetBool ("Punch", true);
+		yield return new WaitForSeconds (timeBeforeFall);
+		rbObject.AddForce (new Vector3 (0, 0, -thugForce));
+		//print ("Die! Pot de fleur");
+	}
+}
--- Assets/Vases/Scripts/Fracture.cs
+++ Assets/Vases/Scripts/Fracture.cs
@@ -2,39 +2,89 @@
 using System.Collections;
 
 public class Fracture : MonoBehaviour {
+	private AudioManager audioManager;
+
 	public Transform parts;
     private Transform breakable = null;
 
+	public GameObject fallingobject;
+	public GameObject mistObj;
+
+	public float timeBeforeFall = 0.2f;
+	public float thugForce = 200f;
+
+	Animator anim;
+	Rigidbody rbObject;
+
+
+	void Start () {
+		audioManager = AudioManager.instance;
+		if (audioManager == null) {
+			Debug.LogError ("Attention le AudioManager n'est pas détecter dans cette scène");
+		}
+
+		anim = mistObj.GetComponent<Animator> ();
+		rbObject = fallingobject.GetComponent<Rigidbody> ();
+	}
+
+
+	void OnTriggerEnter /*Stay*/ (Collider other){
+		if (other.gameObject.tag == "Player" && gameObject.tag == "FallingObjectDroite" /*&& Input.GetButtonDown("XbOne_YButton")*/) {
+			audioManager.PlaySound ("Flower_Woosh");
+			StartCoroutine (MistEstThugGauche());
+		} 
+		if (other.gameObject.tag == "Player" && gameObject.tag == "FallingObjectGauche" /*&& Input.GetButtonDown("XbOne_YButton")*/) {
+			audioManager.PlaySound ("Flower_Woosh");
+			StartCoroutine (MistEstThug());
+		}
+	}
+
+
     private void OnCollisionEnter(Collision collision)
-    {
+	{
 
-        Vector3 velocity = transform.GetComponent<Rigidbody>().velocity;
+		Vector3 velocity = transform.GetComponent<Rigidbody> ().velocity;
         
-        if (velocity.magnitude < 2.0f) return;
-        if (breakable) return;
-        
-        breakable = (Transform)Instantiate(parts, transform.position, transform.rotation); 
-        breakable.localScale = transform.localScale;
+//      if (velocity.magnitude < 2.0f) return;
+//		if (breakable) return;
+		if (collision.gameObject.tag == "Solide" || collision.gameObject.tag == "Gazon") {
+			
+			breakable = (Transform)Instantiate (parts, transform.position, transform.rotation); 
+		//breakable.localScale = transform.localScale;
 
-        foreach (Transform part in breakable)
-        {
-            part.gameObject.GetComponent<Renderer>().materials[1].CopyPropertiesFromMaterial(gameObject.GetComponent<Renderer>().material);
-            if (!part.gameObject.GetComponent<Rigidbody>())
-            {
-                part.gameObject.AddComponent<Rigidbody>();
-                part.gameObject.GetComponent<Rigidbody>().velocity = velocity;
-            }
-            if (!part.gameObject.GetComponent<MeshCollider>())
-            {
-                part.gameObject.AddComponent<MeshCollider>();
-                part.gameObject.GetComponent<MeshCollider>().convex = true;
-            }
+		foreach (Transform part in breakable) {
+			part.gameObject.GetComponent<Renderer> ().materials [1].CopyPropertiesFromMaterial (gameObject.GetComponent<Renderer> ().material);
+			if (!part.gameObject.GetComponent<Rigidbody> ()) {
+				part.gameObject.AddComponent<Rigidbody> ();
+				part.gameObject.GetComponent<Rigidbody> ().velocity = velocity;
+			}
+			if (!part.gameObject.GetComponent<MeshCollider> ()) {
+				part.gameObject.AddComponent<MeshCollider> ();
+				part.gameObject.GetComponent<MeshCollider> ().convex = true;
+			}
 
-            float time = Random.Range(5f, 30f);
-            Destroy(part.gameObject, time);
-        }
+			float time = Random.Range (5f, 30f);
+			Destroy (part.gameObject, time);
+		}
 
-        Destroy(breakable.gameObject, 30f);
-        Destroy(gameObject);
+		Destroy (breakable.gameObject, 30f);
+		Destroy (gameObject);
+		}
     }
+
+	IEnumerator MistEstThug(){
+		anim.SetLayerWeight(3, 1);
+		anim.SetBool ("Punch", true);
+		yield return new WaitForSeconds (timeBeforeFall);
+		rbObject.AddForce (new Vector3 (0, 0, thugForce));
+		//print ("Die! Pot de fleur");
+	}
+
+	IEnumerator MistEstThugGauche(){
+		anim.SetLayerWeight(3, 1);
+		anim.SetBool ("Punch", true);
+		yield return new WaitForSeconds (timeBeforeFall);
+		rbObject.AddForce (new Vector3 (0, 0, -thugForce));
+		//print ("Die! Pot de fleur");
+	}
 }
