cbd2b2225584217541e9fc44ce96562996e29738
AthenaTyrell <36172699+AthenaTyrell@users.noreply.github.com>
2018-04-30 19:01:31 -0400

    AlrightApparementJavaisPushMaisWhatevs

---
+++ Assets/ChangeCheckpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCheckpoint : MonoBehaviour {
+	public GameObject checkpoint;
+	public GameObject referencePoint;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.tag == "Player") {
+			checkpoint.transform.position = referencePoint.transform.position;
+			checkpoint.transform.rotation = referencePoint.transform.rotation;
+
+		}
+
+	}
+}
