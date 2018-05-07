c035738f5af777ef0259a108ce4b0db8f981f988
Maude92 <36176462+Maude92@users.noreply.github.com>
2018-05-07 12:25:16 -0400

    DerniereModifsJePense
    Yay! :D

--- Assets/NeufVies.cs
+++ Assets/NeufVies.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NeufVies : MonoBehaviour {
 
@@ -8,12 +9,15 @@ public class NeufVies : MonoBehaviour {
 
 	public GameObject uiVieObj;
 
+	public Image Circle;
+
 
 	// Use this for initialization
 	void Start () {
 		healthbarscript = uiVieObj.GetComponent<HealthBar> ();
 
 		healthbarscript.NbVieRestant = 9;
+		Circle.fillAmount = 1;
 	}
 	
 	// Update is called once per frame
